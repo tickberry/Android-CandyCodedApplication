f7002bf0a6509c4d1d5952dc07f1d4582187e11d
ochaka ibrahim achaye <47852709+tickberry@users.noreply.github.com>
2019-11-03 19:11:27 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -30,10 +30,11 @@ public class InfoActivity extends AppCompatActivity {
     // ***
 
     public void createMapIntent(View view){
-        Uri uriAddress = Uri.parse("geo:0,0?=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
+        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress );
         mapIntent.setPackage("com.google.android.apps.maps");
-        if(mapIntent.resolveActivity(getPackageManager()) != null) {
+
+        if(mapIntent.resolveActivity(getPackageManager()) != null ) {
             startActivity(mapIntent);
         }
     }
